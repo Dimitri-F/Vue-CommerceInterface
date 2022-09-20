0a3fbee12ed7be2d12db028a3bdd89e0bfcd90c4
Dimitri-F <dimitri.fonsat@gmail.com>
2022-09-20 13:59:09 +0200

    parametrage cookies

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,3 +1,5 @@
+const app = Vue.createApp({})
+
 // data
 const products = [
     { id: 1, description: "Quarz Luxe", price: 12, img: 'assets/img/quarz-luxe.JPG' },
@@ -14,14 +16,13 @@ const products = [
     { id: 12, description: 'Mesh Genova', price: 6, img: 'assets/img/mesh-genova.JPG' },
 ];
 
-// 1. Define route components.
-// These can be imported from other files
+
 const Home = { 
     template: '#home',
     name:'Home',
     data() {
         return {
-            products: products,
+            products,
             searchKey: '',
             liked: []
         }
@@ -31,26 +32,41 @@ const Home = {
             return this.products.filter((product) =>{
                 return product.description.toLowerCase().includes(this.searchKey.toLowerCase());
             })
+        },
+        getLikeCookie() {
+            let cookieValue = JSON.parse($cookies.get('like'))
+            cookieValue == null ? this.liked = [] : this.liked = cookieValue
         }
     },
- 
+    methods: {
+        setLikeCookie() {
+            document.addEventListener('input', () => {
+                setTimeout(() => {
+                    $cookies.set('like', JSON.stringify(this.liked))
+                }, 300);
+            })
+        }
+    },
+    mounted: () => {
+        this.getLikeCookie
+    }
 }
+
 const UserSettings = {
     template: '<h1>UserSettings</h1>',
     name:'UserSettings', 
 }
+
 const WishList = { 
     template: '<h1>WishList</h1>',
     name:'WishList'
 }
+
 const ShoppingCart = {
     template: '<h1>ShoppingCart</h1>',
     name: 'ShoppingCart'
 }
 
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
 const routes = [
     { path: '/', component: Home },
     { path: '/user-settings', component: UserSettings },
@@ -58,18 +74,13 @@ const routes = [
     { path: '/shopping-cart', component: ShoppingCart }
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
     routes, // short for `routes: routes`
 })
 
-// 5. Create and mount the root instance.
-const app = Vue.createApp({})
-// Make sure to _use_ the router instance to make the
-// whole app router-aware.
+
+
 app.use(router)
 
 app.mount('#app')
